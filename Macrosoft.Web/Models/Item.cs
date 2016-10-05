79b15b43c2aea90f5507ed8cc40b4e25108e4b62
sendelany <erancem@gmail.com>
2016-10-05 16:53:26 +0200

    ..

---
+++ Macrosoft.Web/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Macrosoft.Web.Models
+{
+    public class Item
+    {
+        int ParentID { get; set; }
+        string SlideLocation { get; set; }
+        string Icon { get; set; }
+
+    }
+}
